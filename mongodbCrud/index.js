5834c64d147f0f64ff1c02b1a95c71af7f6f4f4e
chaudhuree <chaudhuree00@gmail.com>
2022-11-17 20:17:18 +0600

    docs: find single data

--- mongodbCrud/index.js
+++ mongodbCrud/index.js
@@ -1,11 +1,18 @@
+const chalk = require('chalk');
 const client = require('./dbConnection')
-
+const data = [
+  { name: "shahriar", age: 26, address: { street: 'balighata bazar', city: 'panchbibi' } },
+  { name: "kabir", age: 27, address: { street: 'puratun bazar', city: 'parbatipur' } },
+  { name: "sohan", age: 28, address: { street: 'dakkhinkhan', city: 'dhaka' } }
+]
 // delete a database
-const deleteDatabase = async (db="sohan") => {
+// =================
+// =================
+const deleteDatabase = async (db = "sohan") => {
   try {
     const database = client.db(db)
     await database.dropDatabase()
-    console.log(`database named as: ${db} has been deleted successfully`)
+    console.log(chalk.greenBright.bold(`database named as: ${db} has been deleted successfully`))
   } catch (error) {
     console.error(error)
   } finally {
@@ -14,16 +21,17 @@ const deleteDatabase = async (db="sohan") => {
 }
 
 // to insert single data in database
+// =================================
+// =================================
 const insertSingle = async (db = 'default', collection = 'test') => {
   try {
     const database = client.db(db).collection(collection);
-    // const users = database.collection(collection);
     const doc = {
       name: "sohan chaudhuree",
       age: 27
     }
     const result = await database.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    console.log(chalk.greenBright.bold(`A document was inserted with the _id: ${result.insertedId} in the database: ${client.db(db).s.namespace.db}`));
   } catch (error) {
     console.log(error)
   }
@@ -31,5 +39,63 @@ const insertSingle = async (db = 'default', collection = 'test') => {
     await client.close();
   }
 }
-// insertSingle() //insert database name and collection name in string seperated with comma
+
+// insert multiple data
+// ====================
+// ====================
+
+const insertMultiple = async (db = "multiple", collection = "data", document = data) => {
+  try {
+    const database = client.db(db).collection(collection);
+    const docs = document;
+
+    const result = await database.insertMany(docs);
+    console.log(chalk.greenBright.bold(`${result.insertedCount} documents were inserted in the database ${client.db(db).s.namespace.db}`));
+  } catch (error) {
+    console.log(error)
+  }
+  finally {
+    await client.close();
+  }
+}
+
+
+// find single data
+// ================
+const findSingleData = async (db, collection, queryData = {}) => {
+  try {
+    const database = client.db(db).collection(collection);
+
+    const data = await database.findOne(queryData);
+
+    console.log(chalk.blue.bold(`the searched data is: ${data}`));
+  } catch (error) {
+    console.log(chalk.red.bold(`${error}`));
+
+  } finally {
+    await client.close();
+  }
+}
+
+
+
+
+
+
+
+
+
+
 // deleteDatabase() //insert database name as string
+// deleteDatabase("person")
+
+// insertSingle() //insert database name and collection name in string seperated with comma
+// insertSingle("sdb","scl")
+
+
+// insertMultiple()
+// insertMultiple("mdb","mcl",[{name:'sohan'},{age:27}])
+
+// find single data
+// findSingleData()
+// findSingleData('multiple','data',{name:"kabir"})
