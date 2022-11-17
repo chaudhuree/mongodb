11e3e994b7baf87af68f1b376e3e385571a1c6d1
chaudhuree <chaudhuree00@gmail.com>
2022-11-17 19:36:27 +0600

    docs: drop a database & insertOne data in database

--- mongodbCrud/index.js
+++ mongodbCrud/index.js
@@ -1,5 +1,35 @@
-const { MongoClient } = require("mongodb");
-require('dotenv').config()
-const client = new MongoClient(process.env.DATABASE_URL);
-console.log("database connected")
+const client = require('./dbConnection')
 
+// delete a database
+const deleteDatabase = async (db="sohan") => {
+  try {
+    const database = client.db(db)
+    await database.dropDatabase()
+    console.log(`database named as: ${db} has been deleted successfully`)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.close();
+  }
+}
+
+// to insert single data in database
+const insertSingle = async (db = 'default', collection = 'test') => {
+  try {
+    const database = client.db(db).collection(collection);
+    // const users = database.collection(collection);
+    const doc = {
+      name: "sohan chaudhuree",
+      age: 27
+    }
+    const result = await database.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (error) {
+    console.log(error)
+  }
+  finally {
+    await client.close();
+  }
+}
+// insertSingle() //insert database name and collection name in string seperated with comma
+// deleteDatabase() //insert database name as string
